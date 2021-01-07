2585f0691d9281923cf8ec5b649088d13a66fcdf
nappernick <nickfmatthews@gmail.com>
2021-01-06 19:11:08 -0500

    Making progress on data processing

--- app/api/data_processing.py
+++ app/api/data_processing.py
@@ -2,6 +2,12 @@ import csv
 
 def data_processing(csvPath):
     with open(csvPath) as csvfile:
+        target_columns = ["today_date", "q12", "duration_itw", "q5latitude", "q5longitude", "q9", "duration_itw", "dk_total", "int_outlier_total","check_dist"]
         reader = csv.reader(csvfile, delimiter=",")
-        for row in spamreader:
-            print("\n".join(row))
+        row1 = next(reader)
+        for index, column in enumerate(row1):
+            if column in target_columns:
+                print(column, index)
+
+data_processing("/Users/nicholasmatthews/Library/Mobile Documents/com~apple~CloudDocs/app_academy/capstone/envelope/app/api/my_stata_file.csv")
+
