fd4051bf7f50b769fbad4c622fe6b5af91213928
nappernick <nickfmatthews@gmail.com>
2021-01-16 12:40:46 -0500

    Working on styling the stats page, and adding some functionality

--- react-app/src/components/Stats/StatsPage.js
+++ react-app/src/components/Stats/StatsPage.js
@@ -5,14 +5,29 @@ import AutoSizer from "react-virtualized-auto-sizer";
 function StatsPage() {
     return (
         <div className="stats__page container" style={{ display: "flex", justifyContent: "center" }}>
-            <div className="stats__duration container" style={{ width: "28vw", height: "50vh" }}>
-                <Violinplot statsField={"duration"} />
+            <div className="stats__duration">
+                <div className="stats__duration header">
+                    Duration
+                </div>
+                <div className="stats__duration graph" style={{ width: "28vw", height: "50vh" }}>
+                    <Violinplot statsField={"duration"} />
+                </div>
             </div>
-            <div className="stats__dont_knows container" style={{ width: "28vw", height: "50vh" }}>
-                <Violinplot statsField={"num_dont_know_responses"} />
+            <div className="stats__dont_knows">
+                <div className="stats__dont_knows header">
+                    Don't Know Responses
+                </div>
+                <div className="stats__dont_knows graph" style={{ width: "28vw", height: "50vh" }}>
+                    <Violinplot statsField={"num_dont_know_responses"} />
+                </div>
             </div>
-            <div className="stats__outliers container" style={{ width: "28vw", height: "50vh" }}>
-                <Violinplot statsField={"num_outlier_data_points"} />
+            <div className="stats_outliers">
+                <div className="stats__outliers header">
+                    Integer Outliers
+                </div>
+                <div className="stats__outliers graph" style={{ width: "28vw", height: "50vh" }}>
+                    <Violinplot statsField={"num_outlier_data_points"} />
+                </div>
             </div>
         </div>
     )
