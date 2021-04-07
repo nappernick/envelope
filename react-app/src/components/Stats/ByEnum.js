9b83c5ffe8b9ea4b3380f37dcc7856547c9a6858
nappernick <nickfmatthews@gmail.com>
2021-04-07 19:10:51 -0400

    V1 of tooltip data is complete

--- react-app/src/components/Stats/ByEnum.js
+++ react-app/src/components/Stats/ByEnum.js
@@ -3,7 +3,7 @@ import { Group } from '@visx/group';
 import { ViolinPlot, BoxPlot } from '@vx/stats';
 import { LinearGradient } from '@vx/gradient';
 import { scaleBand, scaleLinear } from '@vx/scale';
-// import { withTooltip, Tooltip, defaultStyles as defaultTooltipStyles } from '@vx/tooltip';
+import { useTooltip, Tooltip, defaultStyles as defaultTooltipStyles } from '@vx/tooltip';
 // import { WithTooltipProvidedProps } from '@vx/tooltip/lib/enhancers/withTooltip';
 import { PatternLines } from '@vx/pattern';
 import { useParams } from 'react-router-dom';
@@ -13,6 +13,15 @@ function ByEnum({ statsField = "duration", h, w }) {
     let { projectId, statsString } = useParams()
     const [dataSetId, setDataSetId] = useState(0)
     const [dataByEnum, setDataByEnum] = useState([])
+
+    const {
+        tooltipData,
+        tooltipLeft,
+        tooltipTop,
+        tooltipOpen,
+        showTooltip,
+        hideTooltip,
+    } = useTooltip();
 
     useEffect(() => {
         const fetchDataSetId = async () => {
@@ -106,13 +115,96 @@ function ByEnum({ statsField = "duration", h, w }) {
                                                     strokeWidth={2}
                                                     valueScale={yScale}
                                                     outliers={boxPlotData.outliers}
-                                                // top={10000000}
+                                                    minProps={{
+                                                        onMouseOver: () => {
+                                                            showTooltip({
+                                                                tooltipTop: yScale(boxPlotData.min) ?? 0 + 40,
+                                                                tooltipLeft: xScale(boxPlotData.enumerator) + constrainedWidth + 5,
+                                                                tooltipData: {
+                                                                    "min": boxPlotData.min,
+                                                                    "name": boxPlotData.enumerator,
+                                                                },
+                                                            });
+                                                        },
+                                                        onMouseLeave: () => {
+                                                            hideTooltip();
+                                                        },
+                                                    }}
+                                                    maxProps={{
+                                                        onMouseOver: () => {
+                                                            showTooltip({
+                                                                tooltipTop: yScale(boxPlotData.max) ?? 0 + 40,
+                                                                tooltipLeft: xScale(boxPlotData.enumerator) + constrainedWidth + 5,
+                                                                tooltipData: {
+                                                                    "max": boxPlotData.max,
+                                                                    "name": boxPlotData.enumerator,
+                                                                },
+                                                            });
+                                                        },
+                                                        onMouseLeave: () => {
+                                                            hideTooltip();
+                                                        },
+                                                    }}
+                                                    boxProps={{
+                                                        onMouseOver: () => {
+                                                            showTooltip({
+                                                                tooltipTop: yScale(boxPlotData.median) ?? 0 + 40,
+                                                                tooltipLeft: xScale(boxPlotData.enumerator) + constrainedWidth + 5,
+                                                                tooltipData: {
+                                                                    ...boxPlotData,
+                                                                    "name": boxPlotData.enumerator,
+                                                                },
+                                                            });
+                                                        },
+                                                        onMouseLeave: () => {
+                                                            hideTooltip();
+                                                        },
+                                                    }}
+                                                    medianProps={{
+                                                        style: {
+                                                            stroke: 'white',
+                                                        },
+                                                        onMouseOver: () => {
+                                                            showTooltip({
+                                                                tooltipTop: yScale(boxPlotData.median) ?? 0 + 40,
+                                                                tooltipLeft: xScale(boxPlotData.median) + constrainedWidth + 5,
+                                                                tooltipData: {
+                                                                    "median": boxPlotData.median,
+                                                                    "name": boxPlotData.enumerator,
+                                                                },
+                                                            });
+                                                        },
+                                                        onMouseLeave: () => {
+                                                            hideTooltip();
+                                                        },
+                                                    }}
                                                 />
                                             </g>
                                         )
                                     })}
                                 </Group>
                             </svg>
+                            {tooltipOpen && tooltipData && (
+                                <Tooltip
+                                    top={tooltipTop}
+                                    left={tooltipLeft}
+                                    style={{ ...defaultTooltipStyles, backgroundColor: '#283238', color: 'white' }}
+                                >
+                                    <div>
+                                        <strong>{tooltipData.name}</strong>
+                                    </div>
+                                    <div style={{ marginTop: '5px', fontSize: '12px' }}>
+                                        {() => {
+                                            return console.log(tooltipData)
+                                        }}
+                                        {tooltipData.max && <div>max: {tooltipData.max}</div>}
+                                        {tooltipData.thirdQuartile && <div>third quartile: {tooltipData.thirdQuartile}</div>}
+                                        {tooltipData.median && <div>median: {tooltipData.median}</div>}
+                                        {tooltipData.firstQuartile && <div>first quartile: {tooltipData.firstQuartile}</div>}
+                                        {tooltipData.min && <div>min: {tooltipData.min}</div>}
+                                    </div>
+                                </Tooltip>
+                            )}
                         </div>
                     )
                 }}
